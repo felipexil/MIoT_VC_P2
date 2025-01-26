d8ba023f68a7e91c067b84df7a4b64b9e63e864a
Felipe Gil-Castiñeira <xil@uvigo.gal>
2025-01-26 14:01:04 +0100

    Update simple_detect.py

--- simple_detect.py
+++ simple_detect.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
 
-# Load the pre-trained model and image
-#model = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'weights.caffemodel')
-
 # Pre-defined class names based on the Caffe model
 class_names = ["background", "aeroplane", "bicycle", "bird", "boat",
                "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
@@ -18,9 +15,7 @@ camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
  
 while True:
-    # frame = picam2.capture_array() # Capture a frame from the camera
     _, frame = camera.read()
-    # frame = cv2.flip(frame, 1) # if your camera reverses your image
     
     img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert the image from RGB to BGR because OpenCV uses BGR by default
     (h, w) = img.shape[:2]  # Get the height and width of the image
